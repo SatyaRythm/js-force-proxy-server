10b74de3da674009ae478215d073f044a4c20c38
SatyaRythm <satyabrata.biswal@rhythminnovations.com>
2025-07-28 22:41:17 +0530

    added res in options parameters

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ import cors from "cors";
  
 const app = express();
 
+let ALLOWED_HEADERS = [
+    'Authorization',
+    'Content-Type',
+    'Salesforceproxy-Endpoint',
+    'X-Authorization',
+    'X-SFDC-Session',
+    'SOAPAction',
+    'Sforce-Auto-Assign',
+    'Sforce-Call-Options',
+    'Sforce-Query-Options',
+    'x-sfdc-packageversion-clientPackage',
+    'If-Modified-Since',
+    'X-User-Agent'
+  ];
+
 // Define CORS options
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    allowedHeaders: ALLOWED_HEADERS,
     credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204
@@ -21,10 +36,16 @@ app.use(express.json());
 app.set('port', process.env.PORT || 8090);
 
 // Handle OPTIONS requests explicitly
-app.options('/proxy', cors(corsOptions));
+app.options('/proxy', cors(corsOptions), (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', ALLOWED_HEADERS.join(','));
+    res.header('Access-Control-Expose-Headers', 'SForce-Limit-Info');
+    res.sendStatus(200);
+});
 
 // JSforce AJAX Proxy route with CORS enabled
-app.all('/proxy', cors(corsOptions), jsforceAjaxProxy({ allowedOrigin: 'http://localhost:3000', enableCORS: true }));
+app.all('/proxy', cors(corsOptions), jsforceAjaxProxy({ enableCORS: true }));
 
 app.get('/', function(req, res) {
   res.send('JSforce AJAX Proxy');
